20df786ebb69d95bd2bc90bf8fd0a3d698743882
Othman AMD <red.fantastic@hotmail.com>
2017-09-20 16:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  // if (cardNumber == null || cardNumber == NaN ){
-  //   console.log("Sorry, we don't have a credit card on file for you.");
-  // }
-  // else{
-  //   console.log("Your total cost is ${71}, which will be charged to the card 83296759.");
-  // }
+  if (cardNumber == null || cardNumber == NaN ){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else{
+    console.log("Your total cost is #{total()}, which will be charged to the card #{cardNumber}.");
+  }
 }
