f8b13ee9fd809c5e39449c384dc4efac0e8bb41d
Othman AMD <red.fantastic@hotmail.com>
2017-09-20 15:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item;
+ var randomPrice = Math.floor((Math.random()*100));
+ var myObject = new Object({[itemName]:randomPrice});
+ cart.push(myObject);
+ console.log(itemName+" has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+if (cart.length === 0){
+  console.log("Your shopping cart is empty.")
+}
+var items =[];
+for(var i=0;i<cart.length;i++){
+  var currentObject = cart[i]
+  for(var key in currentObject){
+    items.push(key+" at $"+currentObject[key]);
+  }
+}
+var info = "In your cart, you have ";
+if(cart.length === 1){
+  console.log(info + items+".");
+}
+if(cart.length === 2){
+  console.log(info+ items[0]+" and "+items[1]+'.');
+}
+else{
+ // var lastItem = items[items.length-1];
+  cosole.log("Fuk u");
+}
+
 }
 
 function total() {
@@ -27,4 +55,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  // if (cardNumber == null || cardNumber == NaN ){
+  //   console.log("Sorry, we don't have a credit card on file for you.");
+  // }
+  // else{
+  //   console.log("Your total cost is ${71}, which will be charged to the card 83296759.");
+  // }
 }
